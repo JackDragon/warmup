46026ac34b6bf02095e863334d540d25d223ce98
Jack Long <r.jack.long@gmail.com>
2014-09-27 19:11:31 -0700

    temp commit, rollback

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,53 @@ class UsersController < ApplicationController
     p params
     p "*"*30
 
+    respond_to do |format|
+      format.html do
+        u = params[:u]
+        user = u[:user]
+        pw = u[:password]
+        type = params[:commit]
+        if type == "login"
+          u = User.authenticate(user, pw)
+          if u
+            @user = u
+            @user.count = @user.count+1
+            @user.save
+            render 'count'
+          else
+            redirect_to action: :sign_in
+          end
+        else
+          # user = create_from_args(user, pw)
+          user = User.new(:user => user, :password => pw, :count => 0)
+          if user.save
+            @user = user
+            render 'index'
+          else
+            errCode = user.errors.first[1]
+            render html: errCode
+            # render 'index'
+          end
+        end
+      end
+      format.json do
+        user = params[:user]
+        pw = params[:password]
+        user = User.new(:user => user, :password => pw, :count => 0)
+        if user.save
+          render json: {errCode: 1, count: 0}
+        else
+          errCode = user.errors.first[1].to_s
+          # p "ERRCODE: ", errCode
+          if errCode == "can't be blank"
+            render json: {errCode: -3}
+          else
+            errCode = errCode.to_i
+            render json: {errCode: errCode}
+          end
+        end
+      end
+    end
     u = params[:u]
     user = u[:user]
     pw = u[:password]
