0b3dde90d6e5d7f3c8ca9e7955d8d17c0950c410
Jack Long <r.jack.long@gmail.com>
2014-10-03 18:19:06 -0700

    UI Javascript and html v2

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
     $('#pw_field').hover(function(){
         $("#pw_field").css("background-color", "#42FF9B");
     }, function(){
-        $("#pw_field").css("background-color", "#E1E1EE");
+        $("#pw_field").css("background-color", "transparent");
     });
     $('#user_field').hover(function(){
         $("#user_field").css("background-color", "#43EFAB");
     }, function(){
-        $("#user_field").css("background-color", "#E1E1EE");
+        $("#user_field").css("background-color", 'transparent');
     });
 });
